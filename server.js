f8f64211fb4fa5d36023cb79754bf1cc2a61e3fe
Possibly Mason N. <135172681+PrimeKidMN@users.noreply.github.com>
2024-10-27 20:39:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,59 +1,250 @@
-{
-  "name": "worldguessr",
-  "version": "0.1.0",
-  "private": true,
-  "type": "module",
-  "scripts": {
-    "dev": "node welcome.js && npx concurrently \"cross-env nodemon ws/ws --exec \"node --no-warnings=ExperimentalWarning\" --watch ws/ws.js\" \"cross-env nodemon server.js --exec \"node --no-warnings=ExperimentalWarning\" --watch server.js\" \"npx next dev\"",
-    "build": "cross-env next build",
-    "start-api": "cross-env NODE_ENV=production node server.js",
-    "start-ws": "cross-env NODE_ENV=production node ws/ws",
-    "start": "cross-env NODE_ENV=production npx concurrently \"node server.js\" \"npx serve out\"",
-    "heroku-postbuild": "cross-env next build"
-  },
-  "dependencies": {
-    "@googlemaps/js-api-loader": "^1.16.6",
-    "@react-oauth/google": "^0.12.1",
-    "@smastrom/react-rating": "^1.5.0",
-    "axios": "^1.7.7",
-    "bad-words": "^4.0.0",
-    "blocked-at": "^1.2.0",
-    "body-parser": "^1.20.3",
-    "colors": "^1.4.0",
-    "coordinate_to_country": "^1.1.0",
-    "cors": "^2.8.5",
-    "countries-code": "^1.1.1",
-    "cross-env": "^7.0.3",
-    "discord-webhook-node": "^1.1.8",
-    "dotenv": "^16.4.5",
-    "express": "^4.21.1",
-    "formidable": "^3.5.1",
-    "google-auth-library": "^9.14.2",
-    "jsonwebtoken": "^9.0.2",
-    "moment": "^2.30.1",
-    "moment-timezone": "^0.5.45",
-    "mongoose": "^8.3.3",
-    "next": "14.2.1",
-    "nextjs-google-analytics": "^2.3.3",
-    "ol": "^9.1.0",
-    "react": "^18.2.0",
-    "react-chatbot-kit": "^2.2.2",
-    "react-dom": "^18.2.0",
-    "react-horizontal-scrolling-menu": "^7.1.1",
-    "react-icons": "^5.1.0",
-    "react-leaflet": "^4.2.1",
-    "react-responsive-modal": "^6.4.2",
-    "react-simple-typewriter": "^5.0.1",
-    "react-toastify": "^10.0.5",
-    "sass": "^1.77.2",
-    "sharp": "^0.33.5",
-    "uWebSockets.js": "github:uNetworking/uWebSockets.js#v20.48.0",
-    "uuid": "^9.0.1"
-  },
-  "devDependencies": {
-    "concurrently": "^9.0.1",
-    "eslint": "^8",
-    "eslint-config-next": "14.2.1",
-    "nodemon": "^3.1.3"
+/*
+                    _     _
+__      _____  _ __| | __| | __ _ _   _  ___  ___ ___ _ __
+\ \ /\ / / _ \| '__| |/ _` |/ _` | | | |/ _ \/ __/ __| '__|
+ \ V  V / (_) | |  | | (_| | (_| | |_| |  __/\__ \__ \ |
+  \_/\_/ \___/|_|  |_|\__,_|\__, |\__,_|\___||___/___/_|  API
+                            |___/
+A game by Gautam
+
+https://github.com/codergautam/worldguessr
+*/
+config();
+
+import mongoose from 'mongoose';
+import Clue from './models/Clue.js';
+import findLatLongRandom from './components/findLatLongServer.js';
+import path from 'path';
+import MapModel from './models/Map.js';
+import bodyParser from 'body-parser';
+
+// express
+import express from 'express';
+var app = express();
+
+// disable cors
+import cors from 'cors';
+import cityGen from './serverUtils/cityGen.js';
+
+app.use(cors());
+app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.urlencoded({limit: '5mb', extended: true, parameterLimit:50000}));
+
+// Setup  /api routes
+const apiFolder = path.join(__dirname, 'api');
+function loadFolder(folder, subdir = '') {
+  fs.readdirSync(folder).forEach(file => {
+    const filePath = path.join(folder, file);
+    if(fs.lstatSync(filePath).isDirectory()) {
+      loadFolder(filePath, subdir + file + '/');
+      return;
+    }
+    if(!file.endsWith('.js')) {
+      return;
+    }
+
+    const routePath = './api/' + subdir + file.split('.')[0]+'.js';
+    const webPath = '/api/' + subdir + file.split('.')[0];
+    import(routePath).then(module => {
+      app.all(webPath, ( req, res ) => {
+        module.default(req, res);
+      });
+    });
+  });
+}
+
+let dbEnabled = true;
+
+if (!process.env.MONGODB) {
+  console.log("[MISSING-ENV WARN] MONGODB env variable not set".yellow);
+  dbEnabled = false;
+} else {
+  // Connect to MongoDB
+  if (mongoose.connection.readyState !== 1) {
+    try {
+      await mongoose.connect(process.env.MONGODB);
+      console.log('[INFO] Database Connected');
+    } catch (error) {
+      console.error('[ERROR] Database connection failed!'.red, error.message);
+      console.log(error);
+      dbEnabled = false;
+    }
   }
 }
+
+
+if(process.env.DISCORD_WEBHOOK) {
+  console.log("[INFO] Discord Webhook Enabled");
+}
+if(!process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID) {
+  console.log("[MISSING-ENV WARN] NEXT_PUBLIC_GOOGLE_CLIENT_ID env variable not set, please set it for multiplayer/auth!".yellow);
+  dbEnabled = false;
+}
+if(!process.env.GOOGLE_CLIENT_SECRET) {
+  console.log("[MISSING-ENV WARN] GOOGLE_CLIENT_SECRET env variable not set, please set it for multiplayer/auth!".yellow);
+  dbEnabled = false;
+}
+
+
+
+
+const port = process.env.API_PORT || 3001;
+
+let recentPlays = {}; // track the recent play gains of maps
+
+async function updateRecentPlays() {
+  if(!dbEnabled) return;
+  for(const mapSlug of Object.keys(recentPlays)) {
+    if(recentPlays[mapSlug] > 0) {
+
+      const map = await MapModel.findOne({ slug: mapSlug });
+      if(map && map.accepted) {
+        map.plays += recentPlays[mapSlug];
+        console.log('Updating plays for', mapSlug, 'by', recentPlays[mapSlug]);
+        await map.save();
+      }
+    }
+  }
+  recentPlays = {};
+}
+
+setInterval(updateRecentPlays, 60000);
+
+
+let allLocations = [];
+let clueLocations = [];
+const locationCnt = 2000;
+const batchSize = 20;
+
+
+const generateMainLocations = async () => {
+  for (let i = 0; i < locationCnt; i += batchSize) {
+    const batchPromises = [];
+
+    for (let j = 0; j < batchSize && i + j < locationCnt; j++) {
+      const locationPromise = new Promise((resolve, reject) => {
+        findLatLongRandom({ location: 'all' }, cityGen, lookup).then(resolve).catch(reject)
+      });
+
+      batchPromises.push(locationPromise);
+    }
+
+    try {
+      const batchResults = await Promise.all(batchPromises);
+      allLocations.push(...batchResults);
+      if(allLocations.length % 100 === 0) console.log('Generated', allLocations.length, '/', locationCnt);
+      if(allLocations.length === locationCnt) {
+        console.log('Finished generating all locations');
+        while (true) {
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+          let latLong;
+          try {
+          latLong = await findLatLongRandom({ location: 'all' }, cityGen, lookup);
+          }catch(e){}
+          // console.log('Generated', latLong);
+          if(!latLong) {
+            continue;
+          }
+          // put in first allLocations and remove the last
+          allLocations.unshift(latLong);
+          allLocations.pop();
+
+        }
+      }
+    } catch (error) {
+      console.error('Error generating batch', i / batchSize, error);
+      generateMainLocations();
+    }
+  }
+};
+
+generateMainLocations();
+
+
+// clue locations
+// get all clues
+const generateClueLocations = async () => {
+  if(!dbEnabled) return;
+  const clues = await Clue.find({});
+  // remove duplicate latlong
+  const uniqueClues = [];
+  let uniqueLatLongs = new Set();
+  for(const clue of clues) {
+    const latLong = `${clue.lat},${clue.lng}`;
+    if(!uniqueLatLongs.has(latLong)) {
+      uniqueLatLongs.add(latLong);
+      uniqueClues.push(clue);
+    }
+  }
+
+  // shuffle
+  uniqueLatLongs = new Set([...uniqueLatLongs].sort(() => Math.random() - 0.5));
+  console.log('Generating clue locations', uniqueLatLongs.size);
+  // populate clueLocations
+  // ex format: {"lat":17.90240017665545,"long":102.7868538747363,"country":"TH"}
+  clueLocations = [];
+  for(const clue of uniqueClues) {
+    const country = lookup(clue.lat, clue.lng, true)[0];
+    clueLocations.push({
+      lat: clue.lat,
+      lng: clue.lng,
+      country
+    });
+  }
+}
+
+generateClueLocations();
+setTimeout(() => {
+  generateClueLocations();
+}, 20000);
+
+  app.get('/', (req, res) => {
+    res.status(200).send('WorldGuessr API - by Gautam');
+  });
+
+loadFolder(apiFolder);
+
+app.get('/allCountries.json', (req, res) => {
+  if (allLocations.length !== locationCnt) {
+    // send json {ready: false}
+    return res.json({ ready: false });
+  } else {
+    return res.json({ ready: true, locations: allLocations });
+  }
+});
+
+// Endpoint for /clueCountries.json
+app.get('/clueCountries.json', (req, res) => {
+  if (clueLocations.length === 0) {
+    // send json {ready: false}
+    return res.json({ ready: false });
+  } else {
+    return res.json({ ready: true, locations: clueLocations.sort(() => Math.random() - 0.5) });
+  }
+});
+
+app.get('/mapLocations/:slug', async (req, res) => {
+  const slug = req.params.slug;
+  const map = await MapModel.findOne({ slug });
+  if (!map) {
+    return res.status(404).json({ message: 'Map not found' });
+  }
+  res.json({
+    ready: true,
+    locations: map.data,
+    name: map.name,
+    official: map.official,
+    maxDist: map.maxDist
+  });
+});
+
+app.post('/mapPlay/:slug', async (req, res) => {
+  const slug = req.params.slug;
+  recentPlays[slug] = (recentPlays[slug] || 0) + 1;
+  res.send('ok');
+});
+
+// listen at port 3001 or process.env.API_PORT
+app.listen(port, () => {
+  console.log(`[INFO] API Server running on port ${port}`);
+});
